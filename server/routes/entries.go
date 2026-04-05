70c5f69186819646ee0244d2610c2c80ccbdf969
Cécile Duffaut <cecileduffaut21@gmail.com>
2026-04-05 12:31:59 +0200

    feat(entries): GetEntries function added

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -9,7 +9,26 @@ func AddEntry(c *gin.Context) {
 }
 
 func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+	var entries []bson.M
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServer, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServer, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
 }
 
 func GetEntryById(c *gin.Context) {

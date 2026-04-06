ecdd1f38a24cb0635785d332f4b2716112a740bf
Cécile Duffaut <cecileduffaut21@gmail.com>
2026-04-06 23:35:40 +0200

    feat(routes/entries): added addentry()

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -1,11 +1,38 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/cduffaut/calorie-tracker-app/models"
+)
 
 var entryCollection *mongo.Collection = openCollection(Client, "calories")
 
 func AddEntry(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	var entry models.Entry
 
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return 
+	}
+	validationErr := validate.Struct(entry)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+		return
+	}
+	entry.ID = primitive.NewObjectID()
+	result, insertErr := entryCollection.InsertOne(ctx, entry )
+	if insertErr != nil {
+		msg := fmt.Sprintf("order item was not created.")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(insertErr)
+		return 	
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result)
 }
 
 func GetEntries(c *gin.Context) {
